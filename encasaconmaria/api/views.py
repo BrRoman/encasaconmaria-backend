fb5c9da5b3226a9f004f8f3bd28530103e9b0841
BrRoman <nicolas.bancillon@gmail.com>
2026-09-16 17:49:58 +0200

    Added filter by category.

--- encasaconmaria/api/views.py
+++ encasaconmaria/api/views.py
@@ -8,9 +8,25 @@ from .serializers import ProductSerializer
 
 @api_view(['GET', 'POST'])
 def products(request):
-    items = Product.objects.all()
-    items_serialized = ProductSerializer(items, many=True)
-    return Response(
-        items_serialized.data,
-        status=status.HTTP_200_OK,
-    )
+    if request.method == 'GET':
+        items = Product.objects.all()
+        filter = request.query_params.get('filter')
+        if filter:
+            items = items.filter(category__name=filter)
+        items_serialized = ProductSerializer(items, many=True)
+        return Response(
+            items_serialized.data,
+            status=status.HTTP_200_OK,
+        )
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
